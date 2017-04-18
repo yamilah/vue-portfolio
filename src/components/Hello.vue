8a2cf8a42ce7dc6a39e38f40689acef5c817413d
Yamilah Atallah <yami@yamilah.com>
2017-04-18 16:10:50 -0400

    Changed some styling.

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,21 +1,10 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+  <div class="nav">
+    <img src="/src/assets/logo.png">
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li>Work</li>
+      <li>Contact</li>
+      <li>Resume</li>
     </ul>
   </div>
 </template>
@@ -33,21 +22,28 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+
+@import url(https://fonts.googleapis.com/css?family=Cormorant);
+@import url(https://fonts.googleapis.com/css?family=Mukta+Vaani);
+
+$sansSerif: 'Mukta Vaani', sans-serif;
+
+.nav {
+  font-family: $sansSerif;
+  text-transform: uppercase;
+  font-weight: 200;
+  letter-spacing: 1.5px;
+
+  padding: 30px;
+
+  display: flex;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+.nav ul {
+  display: flex;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.nav li {
+  padding-left: 15px;
 }
 </style>
