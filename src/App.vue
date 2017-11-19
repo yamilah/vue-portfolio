732d495af8f45c3f8a7dd8fadf6f284406829f03
Yamilah Atallah <yami@yamilah.com>
2017-11-19 16:47:06 -0500

    Added logo and floater background.

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,18 @@
 
 <script>
 // eslint-disable-next-line
+import Normalize from 'normalize.css/normalize.css'
+
+// eslint-disable-next-line
 import SimpleLineIcons from 'simple-line-icons/css/simple-line-icons.css'
+
 export default {
   name: 'app'
 }
 </script>
 
 <style lang="sass">
-  @import url('https://fonts.googleapis.com/css?family=Poppins')
+  @import url('https://fonts.googleapis.com/css?family=Poppins:300,400,900')
 
   #app
     font-family: 'Poppins', sans-serif
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
-    <img src="../assets/floaters.svg" aria-hidden="true">
-    <h1>Yamilah Atallah</h1>
-    <h2 class="icon-user"></h2>
+    <div class="header">
+      <img v-on:load="resizeCallback" :class="{'floater-loaded': floaterLoaded}" :style="floaterStyles" src="../assets/floaters.svg" class="header-img" aria-hidden="true">
+      <div class="header-copy">
+        <img src="../assets/personal-logo.svg" alt="Yamilah Atallah's personal logo.">
+        <section>
+          <h1>Yamilah Atallah</h1>
+          <h2>Designer & Developer</h2>
+        </section>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,13 +18,87 @@ export default {
   name: 'Main',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      floaterStyles: {},
+      floaterLoaded: false
+    }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      window.addEventListener('resize', this.resizeCallback)
+    })
+  },
+  methods: {
+    resizeCallback () {
+      let floaters = this.$el.querySelector('.header-img')
+
+      let scale = Math.max(
+        window.innerWidth / floaters.width,
+        window.innerHeight / floaters.height
+      )
+      if (scale === Infinity) {
+        setTimeout(this.resizeCallback, 10)
+      } else {
+        this.floaterStyles = {
+          transform: `translate(-50%, -50%) scale(${scale})`
+        }
+        this.floaterLoaded = true
+      }
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="sass" scoped>
+  .header
+    resize: both
+    position: relative
+    width: 100%
+    height: 100vh
+
+    .header-img
+      background: white
+      position: relative
+      left: 50%
+      top: 50%
+      transform: translate(-50%, -50%)
+      transform-origin: center center
+      opacity: 0
+
+    .floater-loaded
+      opacity: 1
+      transition: opacity 1.5s ease
+
+    .header-copy
+      display: flex
+      position: absolute
+      transform: translate(-50%, -50%)
+      left: 50%
+      top: 50%
+
+      img
+        align-self: flex-start
+        padding: 19px 50px 0 0
+        width: 127px
+
+      section
+        display: flex
+        flex-direction: column
+
+        h1
+          text-transform: uppercase
+          font-weight: 900
+          font-size: 60px
+          width: 331px
+          letter-spacing: 6px
+          line-height: 1.4
+
+          margin: 0
+
+        h2
+          font-weight: 300
+          font-size: 30px
+
+          margin: 0
+          padding-top: 10px
 
 </style>
